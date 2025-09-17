0d0e756125be52f79d617c47ffac10af1105fa68
despiegk <kristof@incubaid.com>
2025-09-17 09:07:07 +0200

    ...

--- lib/hero/heroserver/doc_handler.v
+++ lib/hero/heroserver/doc_handler.v
@@ -4,22 +4,10 @@ import veb
 
 @['/doc/:handler_type']
 pub fn (mut server HeroServer) doc_handler(mut ctx Context, handler_type string) veb.Result {
-	// Simplified documentation response
-	html_content := '
-	<!DOCTYPE html>
-	<html>
-	<head>
-		<title>API Documentation</title>
-		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-	</head>
-	<body>
-		<div class="container mt-4">
-			<h1>API Documentation</h1>
-			<p>Documentation will be generated here.</p>
-		</div>
-	</body>
-	</html>
-	'
+
+	//TODO: use the templates doc.html...
+	// use the DocSpec
+	panic("implement")
 	
 	return ctx.html(html_content)
 }
