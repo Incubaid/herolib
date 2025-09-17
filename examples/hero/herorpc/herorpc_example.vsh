56db4a17ab04acb4151490f43e82f1db26f1ef84
despiegk <kristof@incubaid.com>
2025-09-17 09:07:03 +0200

    ...

--- examples/hero/herorpc/herorpc_example.vsh
+++ examples/hero/herorpc/herorpc_example.vsh
@@ -2,6 +2,8 @@
 
 import freeflowuniverse.herolib.hero.heromodels.rpc
 
+// when httpport is set, the rpc will be available over http
+// if 0, then its only available on unix socket /tmp/heromodels
 mut http_port := 9933
 
 if http_port == 0 {
@@ -14,13 +16,19 @@ if http_port == 0 {
 
     nc -U /tmp/heromodels
 
-    then e.g. do 
+    then e.g. do
 
     \{"jsonrpc":"2.0","method":"comment_set","params":{"comment":"Hello world!","parent":0,"author":42},"id":1\}
 
     needs to be on one line for openrpc to work
 
     ')
+} else {
+	println('
+    #to test the discover function:
+	curl -X POST -H "Content-Type: application/json" -d \'\{"jsonrpc":"2.0","method":"rpc.discover","id":1,"params":[]\}\' http://localhost:9933/
+    \'
+	')
 }
 
 rpc.start(http_port: http_port)!
--- lib/schemas/openrpc/custom.v
+++ lib/schemas/openrpc/custom.v
@@ -3,80 +3,80 @@ import json
 import freeflowuniverse.herolib.schemas.jsonschema
 
 
-// In the OpenRPC specification struct
-pub fn (spec OpenRPC) methods_by_object() map[string][]Method {
-	mut grouped := map[string][]Method{}
+// // In the OpenRPC specification struct
+// pub fn (spec OpenRPC) methods_by_object() map[string][]Method {
+// 	mut grouped := map[string][]Method{}
 	
-	for method in spec.methods {
-		// Extract root object from method name (e.g., "calendar.create" -> "calendar")
-		parts := method.name.split('.')
-		root_object := if parts.len > 1 { parts[0] } else { 'general' }
+// 	for method in spec.methods {
+// 		// Extract root object from method name (e.g., "calendar.create" -> "calendar")
+// 		parts := method.name.split('.')
+// 		root_object := if parts.len > 1 { parts[0] } else { 'general' }
 		
-		if root_object !in grouped {
-			grouped[root_object] = []Method{}
-		}
-		grouped[root_object] << method
-	}
+// 		if root_object !in grouped {
+// 			grouped[root_object] = []Method{}
+// 		}
+// 		grouped[root_object] << method
+// 	}
 	
-	return grouped
-}
+// 	return grouped
+// }
 
-pub fn (spec OpenRPC) get_object_description(object_name string) string {
-	// Return description for object if available
-	// Implementation depends on how objects are defined in the spec
-	return ''
-}
+// pub fn (spec OpenRPC) get_object_description(object_name string) string {
+// 	// Return description for object if available
+// 	// Implementation depends on how objects are defined in the spec
+// 	return ''
+// }
 
-pub fn (spec OpenRPC) name_fix() string {
-	return spec.info.title.replace(' ', '_').to_lower()
-}
+// pub fn (spec OpenRPC) name_fix() string {
+// 	return spec.info.title.replace(' ', '_').to_lower()
+// }
 
-// In Method struct
-pub fn (method Method) example() (string, string) {
-	// Generate example call and response
-	// This should create realistic JSON examples based on the method schema
+// // In Method struct
+// pub fn (method Method) example() (string, string) {
+// 	// Generate example call and response
+// 	// This should create realistic JSON examples based on the method schema
 	
-	mut example_params := map[string]string{}
-	for param in method.params {
-		param_schema_ref := param.schema
-		example_params[param.name] = match param_schema_ref {
-			openrpc.ContentDescriptor {
-				match param_schema_ref.schema {
-					jsonschema.Schema {
-						param_schema_ref.schema.example_value()
-					}
-					jsonschema.Reference {
-						''
-					}
-				}
-			}
-			jsonschema.Reference {
-				''
-			}
-		}
-	}
+// 	mut example_params := map[string]string{}
+// 	for param in method.params {
+// 		param_schema_ref := param.schema
+// 		example_params[param.name] = match param_schema_ref {
+// 			openrpc.ContentDescriptor {
+// 				match param_schema_ref.schema {
+// 					jsonschema.Schema {
+// 						param_schema_ref.schema.example_value()
+// 					}
+// 					jsonschema.Reference {
+// 						''
+// 					}
+// 				}
+// 			}
+// 			jsonschema.Reference {
+// 				''
+// 			}
+// 		}
+// 	}
 	
-	example_call := json.encode(example_params)
-	example_response := if method.result is openrpc.ContentDescriptor {
-		result_schema_ref := method.result
-		match result_schema_ref {
-			openrpc.ContentDescriptor {
-				match result_schema_ref.schema {
-					jsonschema.Schema {
-						result_schema_ref.schema.example_value()
-					}
-					jsonschema.Reference {
-						'{"result": "success"}'
-					}
-				}
-			}
-			jsonschema.Reference {
-				'{"result": "success"}'
-			}
-		}
-	} else {
-		'{"result": "success"}'
-	}
+// 	example_call := json.encode(example_params)
+// 	example_response := if method.result is openrpc.ContentDescriptor {
+// 		result_schema_ref := method.result
+// 		match result_schema_ref {
+// 			openrpc.ContentDescriptor {
+// 				match result_schema_ref.schema {
+// 					jsonschema.Schema {
+// 						result_schema_ref.schema.example_value()
+// 					}
+// 					jsonschema.Reference {
+// 						'{"result": "success"}'
+// 					}
+// 				}
+// 			}
+// 			jsonschema.Reference {
+// 				'{"result": "success"}'
+// 			}
+// 		}
+// 	} else {
+// 		'{"result": "success"}'
+// 	}
 	
-	return example_call, example_response
-}
+// 	return example_call, example_response
+// }
