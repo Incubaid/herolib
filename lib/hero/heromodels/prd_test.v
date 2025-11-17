3a05dc8ae04a91ba3e753e6d4bd4438d844f37ba
Mahmoud-Emad <mahmmoud.hassanein@gmail.com>
2025-11-17 15:58:01 +0200

    test: Clear test database before running test
    - Flush the Redis database for a clean test state

--- lib/hero/heromodels/prd_test.v
+++ lib/hero/heromodels/prd_test.v
@@ -186,6 +186,9 @@ fn test_prd_type_name() ! {
 
 fn test_prd_list() ! {
 	mut mydb := db.new_test()!
+	// Clear the test database to ensure clean state
+	mydb.redis.flushdb()!
+
 	mut db_prd := DBPrd{
 		db: &mydb
 	}
